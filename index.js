8ce849d4f9187af5adf86898cc937549c48b9413
Patrick Schadler <patrick.schadler@gmail.com>
2018-04-22 13:22:23 +0200

    added twitter function to create friendship with all followers by one target account

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 var Twitter = require('twitter');
 var config = require('./config.json');
 
-var client = new Twitter(config);
+const client = new Twitter(config);
+const targetAccountName = 'ORF';
+let ids = [];
 
 console.log("Starting Twitter growth application...");
 
-var params = {screen_name: 'patzistar'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
+client.get('followers/ids', {screen_name:targetAccountName}, function(error, data, response){
+    if(error)
+        throw error;
+
+    ids = data.ids;
+
+    console.log(`A total of ${ids.length} accounts was retrieved.`)
+    
+    createFriendships(ids);
 });
+
+function createFriendships(ids){
+    var accountIteration = 0;
+
+    setInterval(function() {
+        if (accountIteration < ids.length) {
+            console.log(`Trying to add ${ids[accountIteration]} as friend.`);
+
+            client.post('friendships/create', {user_id:ids[accountIteration]}, function(error, data, response){
+                if(error)
+                    console.log(error);
+                else
+                    console.log(data);
+            });
+        }
+    
+        else return;
+        accountIteration++;
+    }, 5000);
+}
