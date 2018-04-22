701fbd99cab4d325f6e5d51391a338e722459aa6
Patrick Schadler <patrick.schadler@gmail.com>
2018-04-22 12:39:03 +0200

    added dedicated config.json file and required this in the index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+var Twitter = require('twitter');
+var config = require('./config.json');
+
+var client = new Twitter(config);
+
+console.log("Starting Twitter growth application...");
+
+var params = {screen_name: 'patzistar'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
