13d8bb24d117e2eefb7f968bd58f4d1b9b70ac32
Patrick Schadler <patrick.schadler@gmail.com>
2018-04-23 21:52:34 +0200

    added pug for rendering html, added html page on port 3000 to provide GUI to start following process. Moved following logic to follow.js and made it a module

---
+++ follow.js
@@ -0,0 +1,41 @@
+module.exports = function(accountName){
+    const Twitter = require('twitter');
+    const config = require('./config.json');
+
+    const client = new Twitter(config);
+    const targetAccountName = accountName;
+    let ids = [];
+    
+    console.log("Starting Twitter growth application...");
+    
+    client.get('followers/ids', {screen_name:targetAccountName}, function(error, data, response){
+        if(error)
+            throw error;
+    
+        ids = data.ids;
+    
+        console.log(`A total of ${ids.length} accounts was retrieved.`)
+        
+        createFriendships(ids);
+    });
+    
+    function createFriendships(ids){
+        var accountIteration = 0;
+    
+        setInterval(function() {
+            if (accountIteration < ids.length) {
+                console.log(`Trying to add ${ids[accountIteration]} as friend.`);
+    
+                client.post('friendships/create', {user_id:ids[accountIteration], follow:true}, function(error, data, response){
+                    if(error)
+                        console.log(error);
+                    else
+                        console.log(data);
+                });
+            }
+        
+            else return;
+            accountIteration++;
+        }, 5000);
+    }
+}
--- index.js
+++ index.js
@@ -1,39 +1,22 @@
-var Twitter = require('twitter');
-var config = require('./config.json');
+const express = require('express');
+const bodyParser = require('body-parser');
+const follow = require('./follow');
+var pug = require('pug');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.set('view engine','pug');
 
-const client = new Twitter(config);
-const targetAccountName = 'patzistar';
-let ids = [];
 
-console.log("Starting Twitter growth application...");
 
-client.get('followers/ids', {screen_name:targetAccountName}, function(error, data, response){
-    if(error)
-        throw error;
-
-    ids = data.ids;
-
-    console.log(`A total of ${ids.length} accounts was retrieved.`)
-    
-    createFriendships(ids);
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-function createFriendships(ids){
-    var accountIteration = 0;
+app.post('/follow', function (req, res) {
+    console.log(req.body.account);
+    res.send(`Start following the follower of ${req.body.account}`);
+    follow(req.body.account);
+  })
 
-    setInterval(function() {
-        if (accountIteration < ids.length) {
-            console.log(`Trying to add ${ids[accountIteration]} as friend.`);
-
-            client.post('friendships/create', {user_id:ids[accountIteration], follow:true}, function(error, data, response){
-                if(error)
-                    console.log(error);
-                else
-                    console.log(data);
-            });
-        }
-    
-        else return;
-        accountIteration++;
-    }, 5000);
-}
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
